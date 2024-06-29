697138b4fce05291497ad5f6f5678525bcd8b8d3
Lett <lett@ubuntu-23.10-desktop>
2024-06-29 16:00:27 -0400

    Added a command that updates the delay and a configuration file is now required.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Spawn Delay Plugin
 The below will assume that you have some experiences in using the Mindustry server application.
 ## How to use the plugin
-1. Copy the source code into a git repository -- whether local or remote.
-2. Change the variables `serverName` and `delaySeconds` inside of the `src/build_delay/Main.java` file into the desired
-values. `serverName` is the name of your server and `delaySeconds` is the delay relative to the player's join time
-before their team is switched to sharded.
-3. cd into the root of the repository.
-4. Run `./gradlew jar` (or whatever is appropriate for your system).
-5. Copy the file `build/libs/build_delay.jar` into your server's mod folder.
-6. Run or restart the server and ensure that everything is working properly.
+1. Download the `.jar` file from releases.
+2. Copy the `.jar` into your server's mod directory.
+3. Create a new file called `spawn_delay_config.json`.
+4. Copy the below json into your file: ```json
+{
+  "serverName": "SERVER NAME",
+  "delayTime":  5
+}
+```
+5. Ensure that the values are what you intend them to be.
+6. Start or restart (whatever is best applicable) your Mindustry server and ensure that it works properly.
 
 If you have any issues with using or implementing the plugin, then please open an issue. 
--- src/build_delay/Main.java
+++ src/build_delay/Main.java
@@ -1,31 +1,94 @@
 package build_delay;
 
 import arc.*;
+import arc.util.CommandHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import mindustry.game.EventType.*;
 import mindustry.game.Team;
+import mindustry.gen.Player;
 import mindustry.mod.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Main extends Plugin {
-    public final String serverName = "[orange]mindustry.ddns.net[white]";
-    public final Integer delaySeconds = 60;
+    private static String serverName;
+    private static Integer delaySeconds;
+    private final String pluginMessageName = "[gray]<[#003ec8]Spawn Delay Plugin[gray]>[white]";
 
     private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final File configFile = new File("config/mods/spawn_delay_config.json");
+
+    public void UpdateDelay(Integer newDelay) {
+        ObjectNode jsonNode = objectMapper.createObjectNode();
+
+        delaySeconds = newDelay;
+        jsonNode.put("delayTime", newDelay);
+        try {
+            objectMapper.writeValue(configFile, jsonNode);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void LoadConfiguration() throws IOException {
+        JsonNode jsonNode = objectMapper.readTree(configFile);
+
+        serverName = jsonNode.get("serverName").asText("NON-CONFIGURED CONFIG FILE");
+        Integer newDelay = jsonNode.get("delayTime").asInt(5);
+        UpdateDelay(newDelay);
+    }
+
     @Override
     public void init(){
+        try {
+            LoadConfiguration();
+        } catch (IOException e) {
+            delaySeconds = 0;
+            serverName = "UNABLE TO LOAD CONFIGURATION";
+        }
+
         Events.on(PlayerJoin.class, event -> {
             event.player.team(Team.derelict);
 
-            String joinMessage = String.format("[gray]<[#003ec8]Spawn Delay Plugin[gray]>[white] Welcome to " +
+            String joinMessage = String.format("%s Welcome to " +
                     "%s! Please wait %d seconds before you can start interacting with the server. " +
-                    "This is to ensure the safety of players' experiences.", serverName, delaySeconds);
+                    "This is to ensure the safety of players' experiences.", pluginMessageName, serverName, delaySeconds);
             event.player.sendMessage(joinMessage);
 
             ChangeTeamFuture futureTask = new ChangeTeamFuture(event.player);
             executor.schedule(futureTask, delaySeconds, TimeUnit.SECONDS);
+        });
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+        handler.<Player>register("update_delay", "<text...>", "Update the spawn delay.", (args,
+                                                                                        player) -> {
+            if (!player.admin) {
+                String noPermissionString = String.format("%s You do not have the permission to run this command.",
+                        pluginMessageName);
+                player.sendMessage(noPermissionString);
+                return;
+            }
+            try {
+                Integer newDelay = Integer.valueOf(args[0]);
+                UpdateDelay(newDelay);
+
+                String successString = String.format("%s Delay was successfully updated.", pluginMessageName);
+                player.sendMessage(successString);
+            } catch (NumberFormatException e) {
+                String invalidValueString = String.format("%s The value that you provided is not a valid integer.",
+                        pluginMessageName);
+                player.sendMessage(invalidValueString);
+            }
         });
     }
 }
